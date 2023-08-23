cb971c76c765d44a06ad8d1499b5f2ee3dc4a84c
hefamba <81347556+hefamba@users.noreply.github.com>
2023-08-23 09:25:54 -0500

    completed

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const textarea = document.getElementById('textarea');
 textarea.focus();
 
 const createTags = (input) => {
-    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
 
     tagsEl.innerHTML = '';
 
@@ -16,7 +16,54 @@ const createTags = (input) => {
     });
 };
 
+const randomSelect = () => {
+    const times = 30;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unhighlightTag(randomTag);
+        }, 100);
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag);
+        }, 100);
+
+    }, times * 100);
+};
+
+const pickRandomTag = () => {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)];
+};
+
+
+function highlightTag(tag) {
+    tag.classList.add('highlight');
+}
+
+function unhighlightTag(tag) {
+    tag.classList.remove('highlight');
+}
+
+
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value);
+
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = '';
+        }, 10);
+
+        randomSelect();
+    }
 })
 
